02390fde976c6c19b5ae626621ec04868d62d486
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2025-09-12 05:22:22 +0530

    clerk issue resolved

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center 
+          {/* <header className="flex justify-end items-center 
             p-4 gap-4 h-16 
           ">
             <SignedOut>
@@ -57,7 +57,7 @@ export default function RootLayout({
              <SignedIn >
               <UserButton/>
             </SignedIn>
-          </header>
+          </header> */}
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
