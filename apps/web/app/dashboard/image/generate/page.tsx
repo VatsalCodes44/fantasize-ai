c2253205a1ce9197c4574462fe6d84dd5ecb71f9
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2025-09-16 17:27:24 +0530

    minor issue fixed

--- apps/web/app/dashboard/image/generate/page.tsx
+++ apps/web/app/dashboard/image/generate/page.tsx
@@ -2,7 +2,7 @@ import PrismaClient from "@repo/db/client";
 import { auth } from "@clerk/nextjs/server";
 import GenerateImage from "@/components/manualComponents/GenerateImage";
 
-export default async function ({FAIs}: {FAIs: number}) {
+export default async function () {
   const {userId} = await auth()
   if (!userId) return <div>Please sign in to generate images.</div>
   const models = await PrismaClient.model.findMany({
