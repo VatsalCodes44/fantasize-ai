6587e00cf659a204c8ce554a6b4c78412f5edd82
Vatsal Mahajan <mahajanvatsal44@gmail.com>
2025-11-20 01:02:36 +0530

    update

---
+++ apps/web/vercel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install dependencies at root
+npm i
+
+# Generate Prisma Client
+cd packages/db
+npx prisma generate
+cd ../..
+
+# Build the Next.js app
+cd apps/web
+npm run build
+
+# Start Next.js production server
+next start
